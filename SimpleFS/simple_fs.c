6b4a354e271a1577b05aa08cb2aec79822f1f626
Stefano Formicola <stefano_17sf@libero.it>
2017-07-09 23:40:51 +0200

    Modifica a inizialize() Scrittura codice create_dir() più check errori nelle dir

--- SimpleFS/simple_fs.c
+++ SimpleFS/simple_fs.c
@@ -42,17 +42,20 @@ void initialize (inode*, char[], char[]); //nome e dati dell'inode
 
 // } Fine prototipi funzioni
 
+
+
 inode root;
-
-
 
 int main(){
     
     //root=(inode*)malloc(sizeof(inode));
-  
+
     initialize(&root, "/", NULL);
     
-
+    
+    
+    
+    
     
     
     
@@ -74,6 +77,7 @@ void initialize(inode* p,char name[], char data[]){
         // e' una directory
         strcpy(p->name_dir, name);
         p->free_resources=MAX_RES_NUM;
+        p->resources=NULL;
     }
     
     else{
@@ -89,21 +93,42 @@ void create_dir(char path[]){
     inode *curr_dir = &root;
     int tree_height=0;
     int res_num;
+    char sep[2]="/";
+    char *token;
+    int count=0;
     
-    /*
+    /* Segue:
      Parte di codice in cui curr_dir avanza nel percorso
      e 
      tree_height++;
      */
-    
+        token = strtok(path, sep);
+        
+        while((curr_dir->resources!=NULL)&&(token!=NULL)){
+            
+            if (curr_dir->resources[count]==NULL)
+                break; //non trova la directory
+            
+            else if (strcmp(curr_dir->resources[count]->d->name_dir, token)){
+                tree_height++;
+                token=strtok(NULL, sep);
+                curr_dir=curr_dir->resources[count]->d;
+                count=0;
+            }
+            
+            else
+                count++;
+            
+        }
     /*Controllo possibili errori*/
     
-    if (tree_height>=MAX_TREE_HEIGHT)
+    if (token==NULL || curr_dir->resources[count]==NULL || tree_height>=MAX_TREE_HEIGHT) //cartella gia' esistente o non trova la directory o massima altezza nell'albero
         printf("no");
+    
     
     else{
         
-        res_num=MAX_RES_NUM-curr_dir->free_resources;
+        res_num=MAX_RES_NUM-(curr_dir->free_resources);
     /*
      Parte di codice in cui alloco una struct inode per la nuova directory
      
